e2c53e1ecaa52428ec407e2f48b40d9a5570ecb7
ttmzz <112517972+ttmzz@users.noreply.github.com>
2026-02-21 17:23:12 -0300

    Atualizar o typescript-exercicio.js

--- ts/typescript-exercicio.js
+++ ts/typescript-exercicio.js
@@ -1,28 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Calcular {
-    valor1;
-    valor2;
-    constructor(valor1, valor2) {
-        this.valor1 = valor1 * 2;
-        this.valor2 = valor2 * 2;
-    }
-    multiplicador() {
-        return [this.valor1, this.valor2];
-    }
+function multiplicar(a, b) {
+  return a * b;
 }
-const mult = new Calcular(5, 3);
-console.log(mult.multiplicador());
-console.log("------------------------------------");
-class Nome {
-    name;
-    constructor(name) {
-        this.name = name;
-    }
-    saldacao() {
-        return 'Olá ' + `${this.name}`;
-    }
+
+function saudacao(nome) {
+  return `Olá ${nome}`;
 }
-const resp = new Nome('Pedro');
-console.log(resp.saldacao());
-//# sourceMappingURL=typescript-exercicio.js.map
+
+console.log(multiplicar(5, 3)); // 15
+console.log(saudacao("Pedro")); // Olá Pedro
